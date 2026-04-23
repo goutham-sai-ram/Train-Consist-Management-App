3bd5f2e1d6caa07a78b20e0eee766a151e8d109c
goutham-sai-ram <immadisettygouthamsairam@gmail.com>
2026-04-23 15:06:59 +0530

    Goutham Added UC1-InitializeTrain

---
+++ app/src/UseCase1TrainConsistApp.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class UseCase1TrainConsistApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("======================================");
+        System.out.println(" Train Consist Management App ");
+        System.out.println("======================================");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("\nTrain consist initialized.");
+        System.out.println("Initial number of bogies: " + trainConsist.size());
+
+        System.out.println("\nApplication running...");
+    }
+}
